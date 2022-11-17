7675d03d4606901adcce3892c3cfc5886f8873ee
hanyongjun <hanyongjun.dev@gmail.com>
2022-11-17 18:37:54 +0900

    api 로 받은 데이터 를 자식 컴포넌트에 전달성공

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,28 +3,46 @@ import './components/css/App.css';
 import Article from 'components/screens/Article';
 import axios from 'axios';
 
-interface URL {
-  url : string;
+export interface Item {
+  description: string;
+  link: string;
+  originallink: string;
+  pubDate: string;
+  title: string;
 }
-export interface PropsType {
-  data : string
+export interface RootObject {
+  display: number;
+  items: Item[];
+  lastBuildDate: string;
+  start: number;
+  total: number;
+}
+
+interface Proptypes {
+  data : Item[];
 }
 
 function App() {
-  const [count, setCount] = useState<string>('삼성전자');
+  const [count, setCount] = useState<RootObject>({
+    display: 1,
+    items: [],
+    lastBuildDate: "",
+    start: 1,
+    total: 1
+  })
   const [title, setTitle] = useState<string>("삼성전자")
 
   useEffect(()=> {
     const getDatas = async () => {
       const datas = await axios.get(
-        `http://127.0.0.1:8080/${count}`
+        `http://127.0.0.1:8080/${title}`
       )
       setCount(datas.data)
-      // console.log(datas.data)
+      console.log(datas.data)
     }
     getDatas()
   },[])
-  console.log(count)
+  // console.log(count)
 
   return (
     <div className="root">
--- front/src/components/screens/Article.tsx
+++ front/src/components/screens/Article.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import '../css/Article.css'
+import { Item } from 'App'
+import { RootObject } from 'App'
 
-let TestArr = ["제주","강릉","부산","여수","가평","담양","전주"]
+// let TestArr = ["제주","강릉","부산","여수","가평","담양","전주"]
 
-const Article = (props:any) : any => {
-  let articlebox = props.data.map((data:string[],index:number)=>(
-    <div className='articlebox' key={index}>{data}</div>
-  ))
-  return articlebox
+const Article = ({data} : {data:RootObject}) : any => {
+  let articlebox =data.items
+  console.log(articlebox)
+  console.log(typeof articlebox) 
+  articlebox.map((data,index)=> {
+    console.log(data)
+    return (
+      <div className='articlebox' key={index}></div>
+    )
+  })
+  // let arr = []
+  // for(let i =0; i<articlebox.length; i++){
+  //   arr.push(articlebox[i])
+  // }
+
+  // console.log(arr)
+  // let articlebox = data((data:string,index:number)=>(
+  //   <div className='articlebox' key={index}>{data}</div>
+  // ))
+  // return articlebox
 }
 
 export default Article
