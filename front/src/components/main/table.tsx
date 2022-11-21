d4b8870e6eca4c43c196d07c19c755ae2270798a
cho <lll0858630@gmail.com>
2022-11-21 20:36:22 +0900

    -

--- front/src/components/main/table.tsx
+++ front/src/components/main/table.tsx
@@ -1,2 +1,27 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
+
+interface st{
+  name: string;
+  c_prc: string;
+  chg_rt: string;
+  close: string;
+}
+
+
+// const TableMaker =({name, c_prc, chg_rt, close})=> {
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           {columns.map((column) => (
+//             <th key = {column}>{column}</th>
+//           ))}
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {data.map(({''}))}
+//       </tbody>
+//     </table>
+//   )
+// }
