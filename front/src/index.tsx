b1cd95d24c469c48601338e5d7efe2fd483c8d6a
hanyongjun <hanyongjun.dev@gmail.com>
2022-11-24 18:10:36 +0900

    test파일 RouteHub로 이름변경

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './components/css/index.css';
 import App from './App';
-import Main from './test/Main';
+import Main from './RouteHub/Main';
 
 import { BrowserRouter } from 'react-router-dom';
 
