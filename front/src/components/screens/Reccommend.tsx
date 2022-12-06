e18ed0cf2ff74cd3417758527452fed2c216c3f5
LeeSangHo <4leaf.lsh@gmail.com>
2022-12-06 15:17:49 +0900

    매수 추천 색상 변경 수정

--- front/src/components/screens/Reccommend.tsx
+++ front/src/components/screens/Reccommend.tsx
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 interface rec {
-  data : number;
+    data: number;
 }
 
-const Reccomm = ({data}:rec) => {
-  const [backCol, setBackCol] = useState("#bdbdbd");
-  useEffect(() => {
-      if (data >= 1 && data <= 5 && Math.sign(data) === 1) {
-        // 기준가(등락율)이 양수고 1보다 크고 5보다 작으면 추천
-          setBackCol("#ff7676");
-      }
-  });
-  return (
-    <div className="recommend" >
-      <button style={{backgroundColor: backCol}}>매수 적극 추천</button>
-    </div>
-  );
+const Reccomm = ({ data }: rec) => {
+    console.log(data);
+    const [backCol, setBackCol] = useState("#bdbdbd");
+    useEffect(() => {
+        if (data >= 1 && data <= 5 && Math.sign(data) === 1) {
+            // 기준가(등락율)이 양수고 1보다 크고 5보다 작으면 추천
+            setBackCol("#ff7676");
+        } else {
+            setBackCol("#bdbdbd");
+        }
+    }, [data]);
+    return (
+        <div className="recommend">
+            <button style={{ backgroundColor: backCol }}>매수 적극 추천</button>
+        </div>
+    );
 };
 
 export default Reccomm;
